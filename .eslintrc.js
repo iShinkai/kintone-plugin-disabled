bc7fa7dbdb586bb9e71345017ffb781eb77c9805
iShinkai <ishinkai@icloud.com>
2021-02-11 17:59:36 +0900

    [update] build setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
-'use strict';
-
 module.exports = {
-  extends: ['@cybozu/eslint-config/presets/kintone-customize-es5']
-};
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['@cybozu/eslint-config/presets/kintone-customize-es5', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  overrides: [
+    {
+      files: [],
+      env: {},
+    },
+  ],
+}
